8b6116ecc4c92a84c10daea082e54f7a85075ace
willwebster <will.webster@rac.com.au>
2024-12-17 16:27:03 +0800

    More linting fixes

--- code/data_parser1.py
+++ code/data_parser1.py
@@ -1,20 +1,25 @@
+"""Module providing data parsing methods"""
+
+
 class DataParser:
+    """Data Parsers class - file named data_parser1.py to avoid import issues"""
 
     def __init__(self, data):
         self.data = data
 
     def parse(self, stringinput):
+        """ Parse Method"""
         # parsing logic
-        parsedResult = []
-        splitInput = stringinput.split(",")
+        parsed_result = []
+        split_input = stringinput.split(",")
 
-        for x in splitInput:
+        for x in split_input:
             if x == '""':
                 x = '"null"'
 
-            parsedResult.append(x.strip())
+            parsed_result.append(x.strip())
 
-        return parsedResult
+        return parsed_result
 
 # if __name__ == "__main__":
 #     sample_data = "apple,banana,cherry"
